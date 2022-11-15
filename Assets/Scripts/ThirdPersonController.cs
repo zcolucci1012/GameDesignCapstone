b4a5a2934967c9865cc465212e9e5d737bb17b43
Duy Tran <60637571+deantran98@users.noreply.github.com>
2022-11-15 16:45:03 -0500

    Revert "Merge branch 'newbranch' of https://github.com/zcolucci1012/GameDesignCapstone into newbranch"
    This reverts commit 8b6ded3f08491bcfb05b405d0010131896a2c5ba, reversing
    changes made to 5862c9572bc010d0beae1ea6cc26d5285154c16c.

--- Assets/Scripts/ThirdPersonController.cs
+++ Assets/Scripts/ThirdPersonController.cs
@@ -507,10 +507,6 @@ namespace StarterAssets
                 {
                     c.gameObject.GetComponent<EnemyAI>().EnemyHit(4, 25f, this.transform.forward);
                 }
-                else if (c.tag == "Robot")
-                {
-                    c.gameObject.GetComponent<RobotAI>().EnemyHit(4, 25f, this.transform.forward);
-                }
                     
             }
         }
@@ -524,10 +520,6 @@ namespace StarterAssets
                 if (c.tag == "Enemy")
                 {
                     c.gameObject.GetComponent<EnemyAI>().EnemyHit(6, 25f, this.transform.up);
-                }
-                else if (c.tag == "Robot")
-                {
-                    c.gameObject.GetComponent<RobotAI>().EnemyHit(6, 25f, this.transform.up);
                 }
 
             }
@@ -543,10 +535,6 @@ namespace StarterAssets
                 if (c.tag == "Enemy")
                 {
                     c.gameObject.GetComponent<EnemyAI>().EnemyHit(10, 10f, this.transform.forward);
-                }
-                else if (c.tag == "Robot")
-                {
-                    c.gameObject.GetComponent<RobotAI>().EnemyHit(10, 10f, this.transform.forward);
                 }
 
             }
